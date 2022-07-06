9d24c63c7cc2b528d22e960f39803871b0f642ab
MeksD <meksdtotalmedia@gmail.com>
2022-07-05 23:38:53 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*print_alphabet - entry point
+*Return: 0 Success
+*/
+void print_alphabet(void)
+{
+char e;
+for (e = 'a'; e <= 'z'; e++)
+{
+_putchar(e);
+}
+_putchar('\n');
+}
