bcbf712a0536fdc0d36c27ecc28ea6d71a40df77
MeksD <meksdtotalmedia@gmail.com>
2022-07-06 11:40:00 -0700

    Task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+*times_table - prints tables
+*Return: void
+*/
+void times_table(void)
+{
+int a = 0;
+int b;
+int m;
+while (a <= 9)
+{
+b = 0;
+while (b <= 9)
+{
+m = a * b;
+if (b == 0)
+{
+_putchar('0' + m);
+}
+else if (m < 10)
+{
+_putchar(' ');
+_putchar('0' + m);
+}
+else
+{
+_putchar('0' + m / 10);
+_putchar('0' + m % 10);
+}
+if (b < 9)
+{
+_putchar(',');
+_putchar(' ');
+}
+b++;
+}
+_putchar('\n');
+a++;
+}
+}
