b58ed5d799836a54fab484cba041d5eb891fa518
MeksD <meksdtotalmedia@gmail.com>
2022-07-08 22:36:43 -0700

    Task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+*print_triangle - Print a triangle
+*@size: The size of the triangle
+*Return: void.
+*/
+void print_triangle(int size)
+{
+int opp;
+int adj;
+for (opp = 0; opp < size; opp++)
+{
+for (adj = 1; adj < (size - opp); adj++)
+_putchar(' ');
+for (adj--; adj < size; adj++)
+_putchar('#');
+if (opp < (size - 1))
+_putchar('\n');
+}
+_putchar('\n');
+}
