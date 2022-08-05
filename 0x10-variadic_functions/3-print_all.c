00c3a5b3247b0d9c69b5c454c087bfc6d1ee9145
MeksD <meksdtotalmedia@gmail.com>
2022-08-04 23:48:12 -0700

    a function that prints anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -7,13 +7,9 @@ void print_float(va_list arg);
 void print_string(va_list arg);
 void print_all(const char * const format, ...);
 /**
-*
 *print_char - Prints a char.
-*
 *@arg: A list of arguments pointing to
-*
 *the character to be printed.
-*
 */
 void print_char(va_list arg)
 {
@@ -22,13 +18,9 @@ letter = va_arg(arg, int);
 printf("%c", letter);
 }
 /**
-*
 *print_int - Prints an int.
-*
 *@arg: A list of arguments pointing to
-*
-*the integer to be printed.
-*
+*the integer to be printed
 */
 void print_int(va_list arg)
 {
@@ -37,13 +29,9 @@ num = va_arg(arg, int);
 printf("%d", num);
 }
 /**
-*
 *print_float - Prints a float.
-*
 *@arg: A list of arguments pointing to
-*
 *the float to be printed.
-*
 */
 void print_float(va_list arg)
 {
@@ -52,13 +40,9 @@ num = va_arg(arg, double);
 printf("%f", num);
 }
 /**
-*
 *print_string - Prints a string.
-*
 *@arg: A list of arguments pointing to
-*
 *the string to be printed.
-*
 */
 void print_string(va_list arg)
 {
@@ -71,21 +55,12 @@ return;
 printf("%s", str);
 }
 /**
-*
 *print_all - Prints anything, followed by a new line.
-*
 *@format: A string of characters representing the argument types.
-*
 *@...: A variable number of arguments to be printed.
-*
-*
-*
 *Description: Any argument not of type char, int, float,
-*
 *or char * is ignored.
-*
 *If a string argument is NULL, (nil) is printed instead.
-*
 */
 void print_all(const char * const format, ...)
 {
@@ -96,7 +71,7 @@ printer_t funcs[] = {
 {"c", print_char},
 {"i", print_int},
 {"f", print_float},
-{"s", print_string},
+{"s", print_string}
 };
 va_start(args, format);
 while (format && (*(format + i)))
